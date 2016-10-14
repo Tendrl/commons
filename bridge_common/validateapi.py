20b04806397d11649d9827cc71954023cb6be756
Timothy Asir J <tjeyasin@redhat.com>
2016-10-14 13:08:50 +0530

    Validate api-job against sds operation schema
    Fixed pep8 errors
    
    Signed-off-by: Timothy Asir J <tjeyasin@redhat.com>

--- bridge_common/validateapi.py
+++ bridge_common/validateapi.py
@@ -30,7 +30,8 @@ import yaml
 PRIMITIVE_TYPES = {'Boolean': lambda value: isinstance(value, bool),
                    'Float': lambda value: isinstance(value, float),
                    'Integer': lambda value: isinstance(value, int),
-                   'Long': lambda value: isinstance(value, (six.integer_types, float)),
+                   'Long': lambda value: isinstance(value, (
+                       six.integer_types, float)),
                    'String': lambda value: isinstance(value, six.string_types),
                    'Uint': lambda value: isinstance(value, int) and value >= 0,
                    'list': lambda value: isinstance(value, list)}
@@ -57,19 +58,21 @@ def locateSchema(schemaName='operations'):
         raise SchemaNotFound("Unable to find API schema file in %s or %s",
                              localpath, installedpath)
 
-    
+
 def loadSchema(schemaName):
     schemaFile = locateSchema(schemaName)
     try:
         # Check api operation schema file exists
         if not os.path.isfile(schemaFile):
-            return (False, "Error: Schema file '"+ schemaFile + "' does not exists.")
-            
+            return (False,
+                    "Error: Schema file '" + schemaFile + "' does not exists.")
+
         # load api operation schema file and return
-	try:
-	    code = open(schemaFile)
+        try:
+            code = open(schemaFile)
         except IOError, exc:
-            return (return_status, "Error loading schema file '"+ schemaFile + "': " + exc)
+            return (False,
+                    "Error loading schema file '" + schemaFile + "': " + exc)
 
         # Parse schema file
         try:
@@ -77,44 +80,47 @@ def loadSchema(schemaName):
         except yaml.YAMLError, exc:
             error_pos = ""
             if hasattr(exc, 'problem_mark'):
-                mark = exc.problem_mark
-                error_pos = " at position: (%s:%s)" % (exc.problem_mark.line+1,
-                                                       exc.problem_mark.column+1)
-            return (False, "Error loading schema file '"+ SchemaFile + "'" + error_pos \
-                    + ": content format error: Failed to parse yaml format")
+                error_pos = " at position: (%s:%s)" % (
+                    exc.problem_mark.line+1,
+                    exc.problem_mark.column+1)
+            msg = "Error loading schema file '" + schemaFile + "'" + error_pos \
+                + ": content format error: Failed to parse yaml format"
+            return False, msg
 
     except Exception, e:
-        return (False, "Error loading api operation schema file '" \
-                + schemaFile + "': " + str(e))
-                
+        return (False, "Error loading api operation schema file '%s': %s" % (
+            schemaFile, str(e)))
+
     return (True, config)
 
 
 def validateApiWithYamlConf(apiJob, yamlConf):
-    if yamlConf.has_key('valid_objects'):
+    if 'valid_objects' in yamlConf:
         if apiJob['object_type'] not in yamlConf['valid_objects']:
-	    return (False, "Invalid object type or object type not supported!")
+            return (False, "Invalid object type or object type not supported!")
 
     # Check flow exists
-    if apiJob['flow'] not in yamlConf['object_details'][apiJob['object_type']]['atoms']:
-	return (False, "Invalid flow type or flow not supported!")
+    atom = yamlConf['object_details'][apiJob['object_type']]['atoms']
+    if apiJob['flow'] not in atom:
+        return (False, "Invalid flow type or flow not supported!")
 
     # check whether given api-job has any arguments to check
     # or any arguments defined for this purticular job in the yaml.
-    atom = yamlConf['object_details'][apiJob['object_type']]['atoms'][apiJob['flow']]
-    if not atom.has_key('local_inputs') or not apiJob.has_key('attributes'):
+    flow = atom[apiJob['flow']]
+    if 'local_inputs' not in flow or 'attributes' not in apiJob:
         return (True, "")
 
     # check whether any required argument is missing.
     # check the list of arguments given by api-job with the
     # list of required argument mentioned under flow tag in the yaml config.
-    missingInputParm = set(atom['local_inputs']).difference(
+    missingInputParm = set(flow['local_inputs']).difference(
         set(apiJob['attributes'].keys()))
     if missingInputParm != set():
-        return (False, "Missing input argument(s) %s" % (list(missingInputParm)))
+        return (False,
+                "Missing input argument(s) %s" % (list(missingInputParm)))
 
     # check whether all the given arguments are defined in the yaml file.
-    missingConfigParm = set(atom['local_inputs']).difference(
+    missingConfigParm = set(flow['local_inputs']).difference(
         set(apiJob['attributes'].keys()))
     if missingInputParm != set():
         return (False, "Input argument(s) not defined in yaml file: %s" % (
@@ -122,22 +128,23 @@ def validateApiWithYamlConf(apiJob, yamlConf):
 
     # verify whether argument value align with required type.
     # collect the attribute name and its type and check with the
-    # type mentioned in the yaml file for the variable name.  
+    # type mentioned in the yaml file for the variable name.
     attrs = yamlConf['object_details'][apiJob['object_type']]['attrs']
     for inputParm, inputVal in apiJob['attributes'].items():
-        # Some arguments does not required any purticular type. ex:- force, enable
+        # Some arguments does not required any purticular type.
         # Contine validating next parm if the type not defined in yaml.
-        if not attrs.has_key(inputParm):
+        if inputParm not in attrs:
             continue
         if not attrs[inputParm]['type'] in PRIMITIVE_TYPES.keys():
-            return (False, "Invalid parameter type :%s or " \
-                    + "parameter type not supported" % attrs[inputParm]['type'])
+            return (False, "Invalid parameter type :%s" % (
+                attrs[inputParm]['type']))
         # Check the given value type and the required type is matching
         if not PRIMITIVE_TYPES[attrs[inputParm]['type']](inputVal):
-            return (False, "Invalid parameter type: %s. " \
-                    + "Expected value type is: %s" % (inputParm, attrs[inputParm]['type']))
+            return (False, "Invalid parameter type: %s. "
+                    + "Expected value type is: %s" % (
+                        inputParm, attrs[inputParm]['type']))
     return(True, "")
-    
+
 
 class SdsOperations(object):
     def __init__(self):
@@ -161,15 +168,15 @@ class SdsOperations(object):
 
     def validateApi(self, apiJob):
         # Check object type exists
-        if isinstance(apiJob, dict) and not apiJob.has_key('object_type'):
+        if isinstance(apiJob, dict) and 'object_type' not in apiJob:
             return (False, "Invalid request. Object type not found")
 
-        if not apiJob.has_key('flow'):
+        if 'flow' not in apiJob:
             return (False, "Invalid request. Flow name not found")
 
         schema = self.getSchema(apiJob['sds_type'])
         if not schema:
-            return (False, "API Operation Schema file not found " \
+            return (False, "API Operation Schema file not found "
                     + "for the sds type:%s" % apiJob['sds_type'])
 
         return validateApiWithYamlConf(apiJob, schema)
