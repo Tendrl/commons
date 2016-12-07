51d7cb325110fe6f75211909df44319a80752484
anmolbabu <anmolbudugutta@gmail.com>
2016-12-07 17:39:05 +0530

    Update commit message
    tendrl-bug-id: Tendrl/common#69
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- tendrl/common/tests/test_singleton.py
+++ tendrl/common/tests/test_singleton.py
@@ -7,6 +7,15 @@ class A(object):
         pass
 
 
+@to_singleton
+class B(object):
+    def __init__(self):
+        pass
+
+
 class TestSingleton(object):
     def test_singleton(self, monkeypatch):
         assert id(A()) == id(A())
+
+    def test_singletonB(self, monkeypatch):
+        assert id(B()) == id(B())
