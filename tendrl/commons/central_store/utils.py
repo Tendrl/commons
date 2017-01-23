341890bfff2d8a831e48ebb659ce7f31d4918773
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-01-24 02:13:12 +0530

    Update utils.py

--- tendrl/commons/central_store/utils.py
+++ tendrl/commons/central_store/utils.py
@@ -3,8 +3,11 @@ from tendrl.commons.etcdobj import fields
 
 def to_etcdobj(cls_etcd, obj):
     for attr, value in vars(obj).iteritems():
-        if not attr.startswith("_"):
-            setattr(cls_etcd, attr, to_etcd_field(attr, value))
+        if attr.startswith("_"):
+            continue
+        if attr in ["attrs", "enabled", "obj_list", "obj_value", "atoms", "flows"]:
+            continue
+        setattr(cls_etcd, attr, to_etcd_field(attr, value))
     return cls_etcd
 
 
