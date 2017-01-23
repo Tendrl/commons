4db161aa8c7e314852a2761e560b935998f5a96e
Rohan Kanade <rkanade@redhat.com>
2017-01-24 00:56:59 +0530

    - fix atomnotimplemented tendrl-bug-id: Tendrl/specifications#37

--- tendrl/commons/objects/atoms/exceptions.py
+++
@@ -1,9 +0,0 @@
-class AtomNotImplementedError(NotImplementedError):
-    def __init___(self, err):
-        self.message = "run function not implemented. %s".format(err)
-
-
-class AtomExecutionFailedError(Exception):
-    def __init___(self, err):
-        self.message = "Atom Execution failed. Error:" + \
-                       " %s".format(err)
