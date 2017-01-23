3f4452958214e5c6058a64b8ba96a3c1c748cff0
Rohan Kanade <rkanade@redhat.com>
2017-01-23 14:27:55 +0530

    - adds generic statesyncthread and sdssyncthread tendrl-bug-id: Tendrl/specifications#37

--- tendrl/commons/sds_sync/__init__.py
+++ tendrl/commons/sds_sync/__init__.py
@@ -6,7 +6,23 @@ import six
 
 
 @six.add_metaclass(abc.ABCMeta)
-class SdsSyncThread(gevent.greenlet.Greenlet):
+class StateSyncThread(gevent.greenlet.Greenlet):
+    def __init__(self):
+        super(StateSyncThread, self).__init__()
+
+        self._complete = gevent.event.Event()
+
+    def stop(self):
+        self._complete.set()
+
+    @abc.abstractmethod
+    def _run(self):
+        raise NotImplementedError(
+            'define the function run to use this class'
+        )
+
+@six.add_metaclass(abc.ABCMeta)
+class SdsSyncThread(StateSyncThread):
     def __init__(self):
         super(SdsSyncThread, self).__init__()
 
@@ -18,5 +34,5 @@ class SdsSyncThread(gevent.greenlet.Greenlet):
     @abc.abstractmethod
     def _run(self):
         raise NotImplementedError(
-            'define the function on_pull to use this class'
+            'define the function run to use this class'
         )
