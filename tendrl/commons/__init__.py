674d8634cfd06a3ad06ec5af9e279dc85471bb82
Rohan Kanade <rkanade@redhat.com>
2017-02-06 10:53:24 +0530

    - enabled multiple namepsaces per component (required for node-agent import flows)
    tendrl-bug-id: Tendrl/specifications#37

--- tendrl/commons/__init__.py
+++ tendrl/commons/__init__.py
@@ -1,5 +1,10 @@
 __version__ = '1.2'
 
+import importlib
+import inspect
+import pkgutil
+
+
 import namespaces as ns
 
 from tendrl.commons import objects
@@ -15,13 +20,13 @@ class CommonNS(object):
         import __builtin__
         setattr(__builtin__, "tendrl_ns", self)
 
-        ns_str = self.to_str.split(".")[-1]
+        self.ns_str = self.to_str.split(".")[-1]
         # Create the component namespace
-        setattr(self, ns_str,
+        setattr(self, self.ns_str,
                 ns.Namespace(objects=ns.Namespace(), flows=ns.Namespace()))
         self.register_subclasses_to_ns()
 
-        ns_obj = getattr(self, ns_str)
+        ns_obj = getattr(self, self.ns_str)
         # Definitions
         self.definitions = ns_obj.objects.Definition()
 
@@ -46,7 +51,7 @@ class CommonNS(object):
 
     def get_ns(self):
         # eg: input : "tendrl.node_agent", return: "node_agent"
-        return getattr(self, self.to_str.split(".")[-1])
+        return getattr(self, self.ns_str)
 
         # Create the "tendrl_ns.node_agent.objects.$obj.{atoms, flows} NS
     def add_object(self, name, obj_class):
@@ -96,31 +101,65 @@ class CommonNS(object):
     def register_subclasses_to_ns(self):
         # registers all subclasses of BaseObject, BaseFlow, BaseAtom to
         # tendrl_ns
-        def _discover_subclasses(cls, _seen=None):
-            if not isinstance(cls, type):
-                raise TypeError('New-style class required' % cls)
-            if _seen is None: _seen = set()
-            try:
-                subs = cls.__subclasses__()
-            except TypeError:  # fails only when cls is type
-                subs = cls.__subclasses__(cls)
-            for sub in subs:
-                if sub not in _seen:
-                    _seen.add(sub)
-                    yield sub
-                    for sub in _discover_subclasses(sub, _seen):
-                        yield sub
-        for base_class in [objects.BaseObject, flows.BaseFlow, atoms.BaseAtom]:
-            for sub_class in _discover_subclasses(base_class):
-                base_name = base_class.__name__
-                sub_name = sub_class.__name__
-                if "Object" in base_name:
-                    self.add_object(sub_name, sub_class)
-                if "Atom" in base_name:
-                    self.add_atom(sub_class.obj.__name__, sub_name, sub_class)
-                if "Flow" in base_name:
-                    if hasattr(sub_class, "obj"):
-                        self.add_obj_flow(sub_class.obj.__name__, sub_name,
-                                          sub_class)
-                    else:
-                        self.add_flow(sub_name, sub_class)
+        ns_root = importlib.import_module(self.to_str).__path__[0]
+
+        # register objects and atoms, flows inside the objects
+        ns_objects_path = ns_root + "/objects"
+        ns_objects_prefix = self.to_str + ".objects."
+        objs = self._list_modules_in_package_path(ns_objects_path,
+                                                     ns_objects_prefix)
+        for name, obj_fqdn in objs:
+            obj = importlib.import_module(obj_fqdn)
+            for obj_cls in inspect.getmembers(obj, inspect.isclass):
+                if issubclass(obj_cls[1], objects.BaseObject):
+                    obj_name = obj_cls[0]
+                    self.add_object(obj_name, obj_cls[1])
+
+                    ns_object_atoms_path = obj.__path__[0] + "/atoms"
+                    ns_object_atoms_prefix = obj_fqdn + ".atoms."
+                    for atom_name, atom_fqdn in \
+                        self._list_modules_in_package_path(
+                            ns_object_atoms_path,
+                            ns_object_atoms_prefix):
+                        atom = importlib.import_module(atom_fqdn)
+
+                        for atom_cls in inspect.getmembers(atom,
+                                                         inspect.isclass):
+                            if issubclass(atom_cls[1], atoms.BaseAtom):
+                                self.add_atom(atom_cls[1].obj.__name__,
+                                              atom_cls[1].__name__,
+                                              atom_cls[1])
+
+                    ns_object_flows_path = obj.__path__[0] + "/flows"
+                    ns_object_flows_prefix = obj_fqdn + ".flows."
+                    for flow_name, flow_fqdn in \
+                        self._list_modules_in_package_path(
+                            ns_object_flows_path,
+                            ns_object_flows_prefix):
+                        flow = importlib.import_module(flow_fqdn)
+
+                        for flow_cls in inspect.getmembers(flow,
+                                                         inspect.isclass):
+                            if issubclass(flow_cls[1], flows.BaseFlow):
+                                self.add_obj_flow(flow_cls[1].obj.__name__,
+                                                  flow_cls[1].__name__,
+                                                  flow_cls[1])
+
+        ns_flows_path = ns_root + "/flows"
+        ns_flows_prefix = self.to_str + ".flows."
+        flowz = self._list_modules_in_package_path(ns_flows_path,
+                                                   ns_flows_prefix)
+        for name, flow_fqdn in flowz:
+            the_flow = importlib.import_module(flow_fqdn)
+            for flow_cls in inspect.getmembers(the_flow, inspect.isclass):
+                if issubclass(flow_cls[1], flows.BaseFlow):
+                    self.add_flow(flow_cls[0], flow_cls[1])
+
+
+    def _list_modules_in_package_path(self, package_path, prefix):
+        modules = []
+        for importer, name, ispkg in pkgutil.walk_packages(
+                path=[package_path]):
+            modules.append((name, prefix + name))
+        return modules
+
