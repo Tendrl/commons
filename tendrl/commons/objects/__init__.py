7dbb155b7a2a2cef0738d20dc0f9d602c81de34d
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-03-16 19:29:15 +0530

    Update __init__.py

--- tendrl/commons/objects/__init__.py
+++ tendrl/commons/objects/__init__.py
@@ -11,7 +11,8 @@ LOG = logging.getLogger(__name__)
 @six.add_metaclass(abc.ABCMeta)
 class BaseObject(object):
     def __init__(self):
-        self.load_definition()
+        if not hasattr(self, "internal") # Tendrl internal objects should populate their own self._defs
+            self._defs = self.load_definition()
 
     def __new__(cls, *args, **kwargs):
 
@@ -23,20 +24,17 @@ class BaseObject(object):
 
         return instance
 
-    def load_definition(self):
-        LOG.info("Load definitions for namespace.%s.objects.%s" % (self._ns.ns_src, self.__class__.__name__))
+    def load_definition(self, defs=None):
+        LOG.info("Load definitions for namespace.%s.objects.%s" % (self._ns.ns_name,
+                                                                   self.__class__.__name__))
         try:
-            self._defs = self._ns.get_obj_definition(self.__class__.__name__)
+            return self._ns.get_obj_definition(self.__class__.__name__)
         except KeyError as ex:
-            if hasattr(self, "internal"):
-                if self.internal:
-                    self._defs = {}
-                    pass   
-            else:
-                msg = "Could not find definitions for namespace.%s.objects.%s" % (self._ns.ns_src, self.__class__.__name__)
-                LOG.error(ex)
-                LOG.error(msg)
-                raise Exception(msg)
+            msg = "Could not find definitions for namespace.%s.objects.%s" % (self._ns.ns_name,
+                                                                              self.__class__.__name__)
+            LOG.error(ex)
+            LOG.error(msg)
+            raise Exception(msg)
 
 
     def save(self):
@@ -70,27 +68,23 @@ class BaseObject(object):
 class BaseAtom(object):
     def __init__(self, parameters=None):
         self.parameters = parameters
-        self.load_definition()
+        if not hasattr(self, "internal"): # Tendrl internal atoms should populate their own self._defs
+            self._defs = self.load_definition()
 
     def load_definition(self):
-        LOG.info("Load definitions for namespace.%s.objects.%s.atoms.%s" % (self._ns.ns_src,
+        LOG.info("Load definitions for namespace.%s.objects.%s.atoms.%s" % (self._ns.ns_name,
                                                                             self.obj.__name__,
                                                                             self.__class__.__name__))
         try:
-            self._defs = self._ns.get_atom_definition(self.obj.__name__,
+            return self._ns.get_atom_definition(self.obj.__name__,
                                                       self.__class__.__name__)
         except KeyError as ex:
-            if hasattr(self, "internal"):
-                if self.internal:
-                    self._defs = {}
-                    pass   
-            else:
-                msg = "Could not find definitions for namespace.%s.objects.%s.atoms.%s" % (self._ns.ns_src,
+            msg = "Could not find definitions for namespace.%s.objects.%s.atoms.%s" % (self._ns.ns_src,
                                                                             self.obj.__name__,
                                                                             self.__class__.__name__)
-                LOG.error(ex)
-                LOG.error(msg)
-                raise Exception(msg)
+            LOG.error(ex)
+            LOG.error(msg)
+            raise Exception(msg)
 
             
 
