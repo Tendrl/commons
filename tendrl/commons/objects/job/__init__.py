74be203c4a7496bf13921d6e23f55c7bf30e03fa
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-03-29 17:56:37 +0530

    Update __init__.py

--- tendrl/commons/objects/job/__init__.py
+++ tendrl/commons/objects/job/__init__.py
@@ -3,20 +3,37 @@ from tendrl.commons import objects
 
 
 class Job(objects.BaseObject):
+    internal = True
     def __init__(self, job_id=None, payload=None,
-                 status=None, errors=None, *args,
-                 **kwargs):
+                 status=None, errors=None, children=None,
+                 *args, **kwargs):
+        self._defs = {}
         super(Job, self).__init__(*args, **kwargs)
         self.value = 'queue/%s'
         self.job_id = job_id
         self.status = status
         self.payload = payload
         self.errors = errors
+        self.children = children
         self._etcd_cls = _JobEtcd
 
-    def load_definition(self):
-        return {}
-
+    def save(self):
+        payload = json.loads(self.payload)
+        if "parent" in payload:
+            # Load parent job
+            _parent = Job(job_id=payload['parent']).load()
+            _children = []
+            
+            if _parent.children:
+                # Load existing child job ids
+                _children = json.loads(_parent.children)
+                
+            _children += self.job_id
+            _parent.children = _children
+            _parent.save()
+        
+        super(Job, self).save()
+             
 
 class _JobEtcd(etcdobj.EtcdObj):
     # Job etcd object, lazily updated
