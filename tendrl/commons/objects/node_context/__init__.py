cbdbd0311584cc4480906c4e6cc914de8129688d
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-03-31 13:17:51 +0530

    Update __init__.py

--- tendrl/commons/objects/node_context/__init__.py
+++ tendrl/commons/objects/node_context/__init__.py
@@ -1,3 +1,4 @@
+import json
 import os
 import socket
 import sys
@@ -24,7 +25,19 @@ class NodeContext(objects.BaseObject):
         self.machine_id = machine_id or self._get_machine_id()
         self.node_id = node_id or self._get_node_id() or self._create_node_id()
         self.fqdn = fqdn or socket.getfqdn()
-        self.tags = tags or NS.config.data['tags']
+
+        _curr_node_context = self.load()
+        _tags = []
+        try:
+            _tags = json.loads(_curr_node_context.tags)
+        except Valuerror:
+            # No existing tags
+            pass
+        self.tags = tags
+        self.tags += NS.config.data['tags']
+        self.tags += _tags
+        self.tags = list(set(self.tags))
+        
         self.status = status or "UP"
         self._etcd_cls = _NodeContextEtcd
 
