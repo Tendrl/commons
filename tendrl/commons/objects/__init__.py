ab64d78c994d2df5a97f20ddf681bbbbf9aa2a33
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-04-15 05:15:02 +0530

    Update __init__.py

--- tendrl/commons/objects/__init__.py
+++ tendrl/commons/objects/__init__.py
@@ -67,19 +67,20 @@ class BaseObject(object):
             raise Exception(msg)
 
     def save(self, update=True):
-        try:
-            # Generate current in memory object hash
-            cls_etcd = cs_utils.to_etcdobj(self._etcd_cls, self)()
-            self.hash = hash_utils.generate_obj_hash(cls_etcd)
-            cls_etcd.render()
-            _hash_key = "%s/hash" % cls_etcd.__name__
-            _stored_hash = NS.etcd_orm.client.read(_hash_key).value
-            if self.hash == _stored_hash:
-                # No changes in stored object and current object, dont save current object to central store
-                return
-        except (TypeError, etcd.EtcdKeyNotFound):
-            # no hash for this object, save the current hash as is
-            pass
+        if not "Message" in self.__class__.__name__:
+            try:
+                # Generate current in memory object hash
+                cls_etcd = cs_utils.to_etcdobj(self._etcd_cls, self)()
+                self.hash = hash_utils.generate_obj_hash(cls_etcd)
+                cls_etcd.render()
+                _hash_key = "%s/hash" % cls_etcd.__name__
+                _stored_hash = NS.etcd_orm.client.read(_hash_key).value
+                if self.hash == _stored_hash:
+                    # No changes in stored object and current object, dont save current object to central store
+                    return
+            except (TypeError, etcd.EtcdKeyNotFound):
+                # no hash for this object, save the current hash as is
+                pass
         
         self.updated_at = str(time_utils.now())
         if update:
