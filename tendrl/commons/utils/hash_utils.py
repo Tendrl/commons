77ac90fb5b14b3036a7a6e041cb69c1b6733b1b6
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-04-15 05:23:46 +0530

    Update hash_utils.py

--- tendrl/commons/utils/hash_utils.py
+++ tendrl/commons/utils/hash_utils.py
@@ -2,8 +2,11 @@ import hashlib
 import json
 
 def generate_obj_hash(etcd_obj):
-    etcd_obj.hash = None
-    etcd_obj.updated_at = None
+    try:
+        etcd_obj.hash = None
+        etcd_obj.updated_at = None
+    except AttributeError:
+        pass
     # Above items cant be part of hash
     _obj_str = "".join(sorted(etcd_obj.json))
     return hashlib.md5(_obj_str).hexdigest()
