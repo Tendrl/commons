dea82a983cc5432313612d981220efef4f8e55f5
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-04-15 04:45:38 +0530

    Update hash_utils.py

--- tendrl/commons/utils/hash_utils.py
+++ tendrl/commons/utils/hash_utils.py
@@ -1,6 +1,10 @@
 import hashlib
+import json
 
 def generate_obj_hash(etcd_obj):
-    _obj_str = "".join(sorted(etcd_obj.json))
+    _obj = json.loads(etcd_obj.json)
+    _obj.pop("hash", None)
+    _obj.pop("updated_at", None)
+    _obj_str = "".join(sorted(json.dumps(_obj)))
     return hashlib.md5(_obj_str).hexdigest()
         
