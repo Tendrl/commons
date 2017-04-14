37b2a30363b6475c8eb36ab608cf4fe5bbbdf362
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-04-15 05:21:40 +0530

    Update hash_utils.py

--- tendrl/commons/utils/hash_utils.py
+++ tendrl/commons/utils/hash_utils.py
@@ -2,10 +2,9 @@ import hashlib
 import json
 
 def generate_obj_hash(etcd_obj):
-    if hasattr(etcd_obj, "hash"):
-        del etcd_obj.hash
-    if hasattr(etcd_obj, "updated_at"):
-        del etcd_obj.updated_at
+    etcd_obj.hash = None
+    etcd_obj.updated_at = None
+    # Above items cant be part of hash
     _obj_str = "".join(sorted(etcd_obj.json))
     return hashlib.md5(_obj_str).hexdigest()
         
