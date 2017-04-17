3990e0121c8a14ca2d93f6b2e8c9845ed220d234
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-04-17 15:03:20 +0530

    Update ceph_help.py

--- tendrl/commons/flows/create_cluster/ceph_help.py
+++ tendrl/commons/flows/create_cluster/ceph_help.py
@@ -175,4 +175,5 @@ def wait_for_task(task_id):
                     return
     stderr = resp.get("stderr", "ceph-installer task_id %s timed out and did not complete" % task_id)
     stdout = resp.get("stdout", "")
-    raise FlowExecutionFailedError(dict(stdout=stdout, stderr=stderr))
+    raise FlowExecutionFailedError(dict(ceph_installer_task_id=task_id, ceph_installer_task_stdout=stdout,
+                                        ceph_installer_task_stderr=stderr))
