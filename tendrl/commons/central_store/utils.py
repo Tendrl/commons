ffcbd87c6552522206a8fd14643178f8267880c7
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-05-17 13:38:00 +0530

    Update utils.py

--- tendrl/commons/central_store/utils.py
+++ tendrl/commons/central_store/utils.py
@@ -1,4 +1,7 @@
 import datetime
+import time
+
+import etcd
 
 from tendrl.commons.central_store import fields
 
@@ -17,3 +20,25 @@ def to_tendrl_field(name, value, tendrl_type=None):
             return PY_TO_TENDRL_TYPE_MAP[str](name, value)
 
     return PY_TO_TENDRL_TYPE_MAP[type(value)](name, value)
+
+def wreconnect():
+    NS._int.wclient = None
+    while not NS._int.wclient:
+        try:
+            NS._int.wclient = etcd.Client(**NS._int.etcd_kwargs)
+        except etcd.EtcdException:
+            sys.stdout.write(
+                "Error connecting to central store (etcd), trying "
+                "again...")
+            time.sleep(2)
+
+def reconnect():
+    NS._int.client = None
+    while not NS._int.client:
+        try:
+            NS._int.client = etcd.Client(**NS._int.etcd_kwargs)
+        except etcd.EtcdException:
+            sys.stdout.write(
+                "Error connecting to central store (etcd), trying "
+                "again...")
+            time.sleep(2)
