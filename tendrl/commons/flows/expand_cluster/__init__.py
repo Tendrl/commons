061f548b4e8a24b04c1199d5cf547d40e42e8170
Rohan Kanade <rkanade@redhat.com>
2017-05-19 16:33:22 +0530

    Fix python all() returns true even for empty list

--- tendrl/commons/flows/expand_cluster/__init__.py
+++ tendrl/commons/flows/expand_cluster/__init__.py
@@ -48,21 +48,28 @@ class ExpandCluster(flows.BaseFlow):
             if _failed:
                 raise FlowExecutionFailedError("SSH setup failed for jobs %s cluster %s" % (str(_failed),
                                                                                             integration_id))
-            if all([status for status in all_status.values() if status == "finished"]):
-                Event(
-                    Message(
-                        job_id=self.parameters['job_id'],
-                        flow_id=self.parameters['flow_id'],
-                        priority="info",
-                        publisher=NS.publisher_id,
-                        payload={
-                            "message": "SSH setup completed for all "
-                            "nodes in cluster %s" % integration_id
-                        }
+            _finished = []
+            for _status in all_status:
+                if _status == "finished":
+                    _finished.append(True)
+                else:
+                    _finished.append(False)
+            if _finished:
+                if all(_finished):
+                    Event(
+                        Message(
+                            job_id=self.parameters['job_id'],
+                            flow_id=self.parameters['flow_id'],
+                            priority="info",
+                            publisher=NS.publisher_id,
+                            payload={
+                                "message": "SSH setup completed for all "
+                                "nodes in cluster %s" % integration_id
+                            }
+                        )
                     )
-                )
 
-                break
+                    break
 
         # SSH setup jobs finished above, now install sds
         # bits and create cluster
@@ -123,8 +130,9 @@ class ExpandCluster(flows.BaseFlow):
                         detected_cluster, dc)
                 )
 
-            if all([status for status in all_status if status]):
-                break
+            if all_status:
+                if all(all_status):
+                    break
 
         # Create the params list for import cluster flow
         new_params = {}
