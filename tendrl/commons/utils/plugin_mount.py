d60e9f92964586c4ae86fee67bbc59649a92203f
rishubhjain <rishubhjain47@gmail.com>
2017-05-28 05:07:49 +0530

    tendrl-bug-id: Tendrl/commons#547

--- tendrl/commons/utils/plugin_mount.py
+++ tendrl/commons/utils/plugin_mount.py
@@ -1,5 +1,6 @@
 class PluginMount(type):
     def __init__(cls, name, bases, attrs):
+        super(PluginMount, cls).__init__(name, bases, attrs)
         if not hasattr(cls, 'plugins'):
             cls.plugins = []
         else:
