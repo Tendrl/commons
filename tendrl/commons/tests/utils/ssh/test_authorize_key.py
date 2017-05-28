897feb8f9646e95d768c7e90c48c9c3e952fa0a8
rishubhjain <rishubhjain47@gmail.com>
2017-05-29 02:02:40 +0530

    utils/ssh/authorize_key unit test case

---
+++ tendrl/commons/tests/utils/ssh/test_authorize_key.py
@@ -0,0 +1,47 @@
+import pytest
+from tendrl.commons.utils.ssh.authorize_key import AuthorizeKey
+from tendrl.commons.utils import ansible_module_runner
+import mock
+from mock import patch
+
+
+def ansible_run(*args):
+    if args[0]:
+        return {"msg":"test_msg"},""
+    else:
+        return "failed:ssh key not found","Error"
+
+def run(*args):
+    raise ansible_module_runner.AnsibleExecutableGenerationFailed("Error")
+
+
+def ansible(*args,**kwargs):
+    raise ansible_module_runner.AnsibleModuleNotFound
+
+
+def test_constructor():
+    authorize_key = AuthorizeKey("ssh-rsa")
+    assert authorize_key.attributes["user"] == "root"
+    assert authorize_key.attributes["key"] == "ssh-rsa"
+    authorize_key = AuthorizeKey("ssh-rsa","user_name")
+    assert authorize_key.attributes["user"] == "user_name"
+
+
+@mock.patch('tendrl.commons.event.Event.__init__',
+            mock.Mock(return_value=None))
+@mock.patch('tendrl.commons.message.Message.__init__',
+            mock.Mock(return_value=None))
+def test_run():
+    authorize_key = AuthorizeKey("ssh-rsa","user_name")
+    authorize_key.attributes["_raw_params"] = "test_params"
+    with patch.object(ansible_module_runner,'AnsibleRunner',ansible) as mock_ansible:
+        with pytest.raises(ansible_module_runner.AnsibleModuleNotFound):
+            ret = authorize_key.run()
+    with patch.object(ansible_module_runner.AnsibleRunner,'run') as mock_run:
+        mock_run.return_value = ansible_run(True)
+        ret = authorize_key.run()
+        assert ret[0] is True
+    with patch.object(ansible_module_runner.AnsibleRunner,'run') as mock_run:
+        mock_run.return_value = ansible_run(False)
+        ret = authorize_key.run()
+        assert ret[0] is False
