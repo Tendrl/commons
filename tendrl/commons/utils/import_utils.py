37c07fc44613fb1ebce60b3de8c4ac3a5b17c5fd
rishubhjain <rishubhjain47@gmail.com>
2017-05-29 12:47:19 +0530

    tendrl-bug-id: Tendrl/commons#544

--- tendrl/commons/utils/import_utils.py
+++ tendrl/commons/utils/import_utils.py
@@ -1,12 +1,17 @@
 # coding=utf-8
 import importlib
-
+import inspect
 
 def load_abs_module(module_abs_path):
     return importlib.import_module(module_abs_path)
 
 
 def load_abs_class(class_abs_path):
-    kls_name = class_abs_path.split(".")[:-1]
+    kls_name = class_abs_path.split(".")[-1]
     module_name = ".".join(class_abs_path.split(".")[:-1])
-    return getattr(kls_name, load_abs_module(module_name))
+    module = load_abs_module(module_name)
+    obj = inspect.getmembers(module, inspect.isclass)
+    for cls_name,cls_type in obj:
+        if kls_name in cls_name:
+            return cls_type
+    return None
