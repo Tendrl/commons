15ec891268f7fada021c550f667ffc4bc9d9ef3a
rishubhjain <rishubhjain47@gmail.com>
2017-06-01 17:19:34 +0530

    tendrl-bug-id: Tendrl/commons#566

--- tendrl/commons/objects/__init__.py
+++ tendrl/commons/objects/__init__.py
@@ -389,11 +389,11 @@ class BaseAtom(object):
         )
 
 class AtomNotImplementedError(NotImplementedError):
-    def __init___(self, err):
-        self.message = "run function not implemented. %s".format(err)
+    def __init__(self, err):
+        self.message = "run function not implemented. {}".format(err)
 
 
 class AtomExecutionFailedError(Exception):
-    def __init___(self, err):
+    def __init__(self, err):
         self.message = "Atom Execution failed. Error:" + \
-                       " %s".format(err)
+                       " {}".format(err)
