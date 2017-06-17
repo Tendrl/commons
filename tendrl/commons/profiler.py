37be118a4a9a8fdebc3d26b3308d7dafd6c4b101
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-06-18 04:07:45 +0530

    Greenlet function stat profiler
    tendrl-bug-id: Tendrl/commons#612

---
+++ tendrl/commons/profiler.py
@@ -0,0 +1,22 @@
+import sys
+
+def initiate():
+  import atexit
+  import GreenletProfiler
+  
+  GreenletProfiler.set_clock_type('cpu')
+  GreenletProfiler.start()
+  sys.stdout.write("Started Tendrl profiling...")
+
+  @atexit.register
+  def finish_yappi():
+    GreenletProfiler.stop()
+    sys.stdout.write("Stopped Tendrl profiling...")
+    stats = GreenletProfiler.get_func_stats()
+
+    for stat_type in ['pstat', 'callgrind', 'ystat']:
+        _stat_path = "/var/lib/tendrl/profiling/{0}/last_run_func_stat.{1}".formant(NS.publisher_id,
+                                                                                    stat_type)
+        stats.save(_stat_path, type=stat_type)
+        
+    sys.stdout.write("Saved Tendrl profiling stats at ('/var/lib/tendrl/profiling/{0}/'.format(NS.publisher_id))")
