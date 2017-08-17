560879e60e56c1b40e092a1ae5f824bc7f2d17a8
Shubhendu <shtripat@redhat.com>
2017-08-17 10:40:54 +0530

    Incorporated review comments
    tendrl-bug-id: Tendrl/gluster-integration#353
    Signed-off-by: Shubhendu <shtripat@redhat.com>

--- tendrl/commons/objects/cluster/__init__.py
+++ tendrl/commons/objects/cluster/__init__.py
@@ -5,7 +5,7 @@ class Cluster(objects.BaseObject):
     def __init__(self, integration_id=None, public_network=None,
                  cluster_network=None, node_configuration=None,
                  conf_overrides=None, node_identifier=None, sync_status=None,
-                 last_sync=None, is_managed="no",
+                 last_sync=None, is_managed=False,
                  enable_volume_profiling=False, *args, **kwargs):
         super(Cluster, self).__init__(*args, **kwargs)
         self.integration_id = integration_id
