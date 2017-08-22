c7b222f78a780c508a29f4d302bd7b77534013c8
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-08-22 17:39:25 +0530

    Update __init__.py

--- tendrl/commons/objects/cluster/__init__.py
+++ tendrl/commons/objects/cluster/__init__.py
@@ -5,8 +5,8 @@ class Cluster(objects.BaseObject):
     def __init__(self, integration_id=None, public_network=None,
                  cluster_network=None, node_configuration=None,
                  conf_overrides=None, node_identifier=None, sync_status=None,
-                 last_sync=None, is_managed=False,
-                 enable_volume_profiling=False, errors=None, *args, **kwargs):
+                 last_sync=None, is_managed="no",
+                 enable_volume_profiling="no", errors=[], *args, **kwargs):
         super(Cluster, self).__init__(*args, **kwargs)
         self.integration_id = integration_id
         self.public_network = public_network
@@ -18,7 +18,7 @@ class Cluster(objects.BaseObject):
         self.last_sync = last_sync
         self.is_managed = is_managed
         self.enable_volume_profiling = enable_volume_profiling
-        self.errors = errors or []
+        self.errors = errors
         self.value = 'clusters/{0}'
 
     def render(self):
