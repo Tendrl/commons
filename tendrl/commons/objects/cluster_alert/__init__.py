ecbedd014460cc05ee5f7f91a440e7190313703e
GowthamShanmugam <gshanmug@redhat.com>
2017-08-31 14:40:51 +0530

    Optimizing node alert and cluster alert  object defenition
    tendrl-bug-id : Tendrl/commons#707
    
    Signed-off-by: GowthamShanmugam <gshanmug@redhat.com>

--- tendrl/commons/objects/cluster_alert/__init__.py
+++ tendrl/commons/objects/cluster_alert/__init__.py
@@ -4,44 +4,8 @@ from tendrl.commons import objects
 
 
 class ClusterAlert(Alert, objects.BaseObject):
-    def __init__(
-        self,
-        alert_id=None,
-        node_id=None,
-        time_stamp=None,
-        resource=None,
-        current_value=None,
-        tags=None,
-        alert_type=None,
-        severity=None,
-        significance=None,
-        ackedby=None,
-        acked=None,
-        ack_comment=[],
-        acked_at=None,
-        pid=None,
-        source=None,
-        delivered=None,
-        *args,
-        **kwargs
-    ):
+    def __init__(self, *args, **kwargs):
         super(ClusterAlert, self).__init__(
-            alert_id,
-            node_id,
-            time_stamp,
-            resource,
-            current_value,
-            tags,
-            alert_type,
-            severity,
-            significance,
-            ackedby,
-            acked,
-            ack_comment,
-            acked_at,
-            pid,
-            source,
-            delivered,
             *args,
             **kwargs
         )
--- tendrl/commons/objects/node_alert/__init__.py
+++ tendrl/commons/objects/node_alert/__init__.py
@@ -4,46 +4,8 @@ from tendrl.commons import objects
 
 
 class NodeAlert(Alert, objects.BaseObject):
-    def __init__(
-        self,
-        alert_id=None,
-        node_id=None,
-        time_stamp=None,
-        resource=None,
-        current_value=None,
-        tags=None,
-        alert_type=None,
-        severity=None,
-        classification=None,
-        significance=None,
-        ackedby=None,
-        acked=None,
-        ack_comment=[],
-        acked_at=None,
-        pid=None,
-        source=None,
-        delivered=None,
-        *args,
-        **kwargs
-    ):
+    def __init__(self, *args, **kwargs):
         super(NodeAlert, self).__init__(
-            alert_id,
-            node_id,
-            time_stamp,
-            resource,
-            current_value,
-            tags,
-            alert_type,
-            severity,
-            classification,
-            significance,
-            ackedby,
-            acked,
-            ack_comment,
-            acked_at,
-            pid,
-            source,
-            delivered,
             *args,
             **kwargs
         )
