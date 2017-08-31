6187c40ba12807ab7fb14bcf518f9f26288a669a
Shubhendu <shtripat@redhat.com>
2017-08-31 23:25:56 +0530

    Added utility to update dashboard with required details
    tendrl-bug-id: Tendrl/gluster-integration#385
    Signed-off-by: Shubhendu <shtripat@redhat.com>

---
+++ tendrl/commons/utils/monitoring_utils.py
@@ -0,0 +1,27 @@
+import uuid
+
+from tendrl.commons.objects.job import Job
+
+
+def update_dashboard(res_name, res_type, integration_id, action):
+    _job_id = str(uuid.uuid4())
+    _params = {
+        "TendrlContext.integration_id": NS.tendrl_context.integration_id,
+        "Trigger.resource_name": res_name,
+        "Trigger.resource_type": res_type,
+        "Trigger.action": action
+    }
+    _job_payload = {
+        "tags": ["tendrl/integration/monitoring"],
+        "run": "monitoring.flows.UpdateDashboard",
+        "status": "new",
+        "parameters": _params,
+        "type": "monitoring"
+    }
+    Job(
+        job_id=_job_id,
+        status="new",
+        payload=_job_payload
+    ).save()
+
+    return _job_id
