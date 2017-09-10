02998769986cd4a1659a15b7658033184bb4c6fa
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-09-10 22:00:30 +0530

    Update __init__.py

--- tendrl/commons/objects/__init__.py
+++ tendrl/commons/objects/__init__.py
@@ -76,27 +76,9 @@ class BaseObject(object):
     def save(self, update=True, ttl=None):
         self.render()
         if "Message" not in self.__class__.__name__:
-            try:
-                # Generate current in memory object hash
-                self._hash()
-                _hash_key = "/{0}/hash".format(self.value)
-                _stored_hash = None
-                try:
-                    _stored_hash = NS._int.client.read(_hash_key).value
-                except (etcd.EtcdConnectionFailed, etcd.EtcdException) as ex:
-                    if type(ex) != etcd.EtcdKeyNotFound:
-                        NS._int.reconnect()
-                        _stored_hash = NS._int.client.read(_hash_key).value
-                if self.hash == _stored_hash:
-                    # No changes in stored object and current object,
-                    # dont save current object to central store
-                    if ttl:
-                        etcd_utils.refresh(self.value, ttl)
-                    return
-            except TypeError:
-                # no hash for this object, save the current hash as is
-                pass
-
+            # If local object.hash is equal to central_store object.hash, return
+            if self.hash_compare_with_central_store(ttl=ttl):
+                return
         if update:
             current_obj = self.load()
             for attr, val in vars(self).iteritems():
@@ -182,24 +164,9 @@ class BaseObject(object):
 
     def load(self):
         if "Message" not in self.__class__.__name__:
-            try:
-                # Generate current in memory object hash
-                self._hash()
-                _hash_key = "/{0}/hash".format(self.value)
-                _stored_hash = None
-                try:
-                    _stored_hash = NS._int.client.read(_hash_key).value
-                except (etcd.EtcdConnectionFailed, etcd.EtcdException) as ex:
-                    if type(ex) != etcd.EtcdKeyNotFound:
-                        NS._int.reconnect()
-                        _stored_hash = NS._int.client.read(_hash_key).value
-                if self.hash == _stored_hash:
-                    # No changes in stored object and current object,
-                    # dont save current object to central store
-                    return self
-            except TypeError:
-                # no hash for this object, save the current hash as is
-                pass
+            # If local object.hash is equal to central_store object.hash, return
+            if self.hash_compare_with_central_store():
+                return self
 
         _copy = self._copy_vars()
         # Check if self.value already set, use it
@@ -345,8 +312,29 @@ class BaseObject(object):
         self.hash = hashlib.md5(_obj_str).hexdigest()
         return self.hash
     
-    def get_hash(self):
-        return self._hash()
+    def hash_compare_with_central_store(self, ttl=None):
+        try:
+            # Generate current in memory object hash
+            self._hash()
+            _hash_key = "/{0}/hash".format(self.value)
+            _stored_hash = None
+            try:
+                _stored_hash = NS._int.client.read(_hash_key).value
+            except (etcd.EtcdConnectionFailed, etcd.EtcdException) as ex:
+                if type(ex) != etcd.EtcdKeyNotFound:
+                    NS._int.reconnect()
+                    _stored_hash = NS._int.client.read(_hash_key).value
+            if self.hash == _stored_hash:
+                # No changes in stored object and current object,
+                # dont save current object to central store
+                if ttl:
+                    etcd_utils.refresh(self.value, ttl)
+                return True
+            else:
+                return False
+        except TypeError:
+            # no hash for this object, save the current hash as is
+            return False
     
     def _copy_vars(self):
         # Creates a copy intance of $obj using it public vars
