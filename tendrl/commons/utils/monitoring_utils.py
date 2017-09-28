7c1109ac02ee2bb6d3a5b583bbde7430e6fa463e
Shubhendu <shtripat@redhat.com>
2017-09-28 11:37:39 +0530

    Added utility to invoke UpdateGraphite flow from monitoring-integration
    tendrl-bug-id: Tendrl/gluster-integration#437
    Signed-off-by: Shubhendu <shtripat@redhat.com>

--- tendrl/commons/utils/monitoring_utils.py
+++ tendrl/commons/utils/monitoring_utils.py
@@ -26,3 +26,28 @@ def update_dashboard(res_name, res_type, integration_id, action):
     ).save()
 
     return _job_id
+
+
+def update_graphite(res_name, res_type, integration_id, action):
+    _job_id = str(uuid.uuid4())
+    _params = {
+        "TendrlContext.integration_id": NS.tendrl_context.integration_id,
+        "TendrlContext.cluster_name": NS.tendrl_context.cluster_name,
+        "Trigger.resource_name": res_name,
+        "Trigger.resource_type": res_type,
+        "Trigger.action": action
+    }
+    _job_payload = {
+        "tags": ["tendrl/integration/monitoring"],
+        "run": "monitoring.flows.UpdateGraphite",
+        "status": "new",
+        "parameters": _params,
+        "type": "monitoring"
+    }
+    Job(
+        job_id=_job_id,
+        status="new",
+        payload=_job_payload
+    ).save()
+
+    return _job_id
