e739759ae12ec61448eafd1bfad1676368dff4b4
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-10-11 19:49:54 +0530

    Update __init__.py

--- tendrl/commons/jobs/__init__.py
+++ tendrl/commons/jobs/__init__.py
@@ -48,10 +48,7 @@ class JobConsumerThread(threading.Thread):
                 continue
 
             for job in jobs.leaves:
-                _job_thread = threading.Thread(target=process_job, args=(job,))
-                _job_thread.daemon = True
-                _job_thread.start()
-                _job_thread.join()
+                process_job(job)
 
     def stop(self):
         self._complete.set()
