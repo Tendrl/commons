1cf15d6aba8c5f90b10b4611dc18606ee8afe650
GowthamShanmugam <gshanmug@redhat.com>
2018-02-23 02:52:41 +0530

    Failing import or expand or unmanaged cluster flow when any node is down
    tendrl-bug-id: Tendrl/commons#828
    
    Signed-off-by: GowthamShanmugam <gshanmug@redhat.com>

--- tendrl/commons/objects/node/atoms/check_node_up/__init__.py
+++ tendrl/commons/objects/node/atoms/check_node_up/__init__.py
@@ -1,40 +1,81 @@
+import etcd
+import json
+
+from tendrl.commons.flows.exceptions import FlowExecutionFailedError
 from tendrl.commons.objects import BaseAtom
 from tendrl.commons.utils.cmd_utils import Command
+from tendrl.commons.utils import etcd_utils
 from tendrl.commons.utils import log_utils as logger
 
 
 class CheckNodeUp(BaseAtom):
+    def __init__(self, *args, **kwargs):
+        super(CheckNodeUp, self).__init__(*args, **kwargs)
 
     def run(self):
-        fqdn = self.parameters.get("fqdn")
-        logger.log(
-            "info",
-            NS.publisher_id,
-            {"message": "Checking if node %s is up" %
-             self.parameters.get("fqdn")},
-            job_id=self.parameters['job_id'],
-            flow_id=self.parameters['flow_id']
-        )
-        out, err, rc = Command("ping -c 1 %s" % fqdn).run()
-        # and then check the response...
-        if not err and rc == 0:
-            logger.log(
-                "info",
-                NS.publisher_id,
-                {"message": "Ping to %s succeeded." %
-                 self.parameters.get("fqdn")},
-                job_id=self.parameters['job_id'],
-                flow_id=self.parameters['flow_id']
+        try:
+            flag = True
+            # check job is parent or child
+            job = NS.tendrl.objects.Job(
+                job_id=self.parameters['job_id']
+            ).load()
+            if "parent" not in job.payload:
+                # Getting node_ids from parameters
+                node_ids = self.parameters.get(
+                    'Node[]',
+                    None
+                )
+                if not node_ids:
+                    # fetch node id using integration_id
+                    integration_id = self.parameters[
+                        'TendrlContext.integration_id'
+                    ]
+                    key = "indexes/tags/tendrl/integration/%s" % \
+                        integration_id
+                    node_ids_str = etcd_utils.read(key).value
+                    node_ids = json.loads(node_ids_str)
+                # identifying fqdn using node_id
+                hosts = []
+                for node in node_ids:
+                    key = "nodes/%s/NodeContext/fqdn" % node
+                    fqdn = etcd_utils.read(key).value
+                    hosts.append(str(fqdn))
+                logger.log(
+                    "info",
+                    NS.publisher_id,
+                    {"message": "Checking if nodes %s are up" % hosts},
+                    job_id=self.parameters['job_id'],
+                    flow_id=self.parameters['flow_id']
+                )
+                nodes_up = []
+                nodes_down = []
+                for fqdn in hosts:
+                    out, err, rc = Command("ping -c 1 %s" % fqdn).run()
+                    # and then check the response...
+                    if not err and rc == 0:
+                        nodes_up.append(fqdn)
+                    else:
+                        flag = False
+                        nodes_down.append(fqdn)
+                if flag:
+                    logger.log(
+                        "info",
+                        NS.publisher_id,
+                        {"message": "Ping to nodes %s succeeded." % nodes_up},
+                        job_id=self.parameters['job_id'],
+                        flow_id=self.parameters['flow_id']
+                    )
+                else:
+                    logger.log(
+                        "info",
+                        NS.publisher_id,
+                        {"message": "Failed to ping nodes %s" % nodes_down},
+                        job_id=self.parameters['job_id'],
+                        flow_id=self.parameters['flow_id']
+                    )
+            # no need to check for child job
+            return flag
+        except (etcd.EtcdKeyNotFound, KeyError, TypeError) as ex:
+            raise FlowExecutionFailedError(
+                "Error checking status of nodes .error: %s" % str(ex)
             )
-            return True
-        else:
-            logger.log(
-                "info",
-                NS.publisher_id,
-                {"message": "Failed to ping %s. Error %s" % (
-                    self.parameters.get("fqdn"),
-                    err)},
-                job_id=self.parameters['job_id'],
-                flow_id=self.parameters['flow_id']
-            )
-            return False
--- tendrl/commons/tests/objects/node/atoms/check_node_up/test_check_node_up_init.py
+++ tendrl/commons/tests/objects/node/atoms/check_node_up/test_check_node_up_init.py
@@ -1,35 +1,64 @@
 import __builtin__
 import maps
-import mock
 from mock import patch
 
 from tendrl.commons.objects.node.atoms.check_node_up import CheckNodeUp
 from tendrl.commons.utils.cmd_utils import Command
+from tendrl.commons.utils import etcd_utils
+from tendrl.commons.utils import log_utils as logger
+
+
+class MockJob(object):
+    def __init__(self, job_id=None):
+        pass
+
+    def load(self):
+        self.payload = {}
+        return self
+
+
+def false_run(*args):
+    return "Test Out", '', 1
 
 
 def run(*args):
     return "Test Out", '', 0
 
 
+def read(key):
+    if "tags" in key:
+        out = maps.NamedDict(
+            value=u'["fe80532d-95e0-4b10-b486-a357e325cccf"]'
+        )
+    elif "fqdn" in key:
+        out = maps.NamedDict(
+            value=u'dhcp12'
+        )
+    return out
+
+
 # Testing run
-@mock.patch('tendrl.commons.event.Event.__init__',
-            mock.Mock(return_value=None))
-@mock.patch('tendrl.commons.message.Message.__init__',
-            mock.Mock(return_value=None))
-def test_run():
+@patch.object(logger, "log")
+def test_run(log):
+    log.return_value = True
     setattr(__builtin__, "NS", maps.NamedDict())
     NS.publisher_id = 1
     NS["config"] = maps.NamedDict()
     NS.config["data"] = maps.NamedDict(logging_socket_path="test/path")
+    NS["tendrl"] = maps.NamedDict()
+    NS.tendrl["objects"] = maps.NamedDict(Job=MockJob)
     NS.node_context = maps.NamedDict()
     NS.node_context.node_id = 1
     check_node_up = CheckNodeUp()
     check_node_up.parameters = maps.NamedDict()
-    check_node_up.parameters['fqdn'] = "www.google.com"
+    check_node_up.parameters['TendrlContext.integration_id'] = \
+        "7a3f2238-ef79-4943-9edf-762a80cf22a0"
     check_node_up.parameters['job_id'] = "node_job"
     check_node_up.parameters['flow_id'] = "flow_id"
-    check_node_up.run()
-    with patch.object(Command, "run", run):
-        check_node_up.run()
-    check_node_up.parameters['fqdn'] = "Test_fqdn"
-    check_node_up.run()
+    with patch.object(etcd_utils, "read", read):
+        with patch.object(Command, "run", false_run):
+            if check_node_up.run():
+                raise AssertionError
+        with patch.object(Command, "run", run):
+            if not check_node_up.run():
+                raise AssertionError
