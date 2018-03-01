03fbf48f4f07a145955c2cc661a89b1a69e49308
Rohan Kanade <r0h4n@users.noreply.github.com>
2018-03-02 01:36:30 +0530

    Adds attr "ipv4_addr"
    tendrl-bug-id: Tendrl/node-agent#718

--- tendrl/commons/objects/node_context/__init__.py
+++ tendrl/commons/objects/node_context/__init__.py
@@ -17,14 +17,14 @@ NODE_ID = None
 
 class NodeContext(objects.BaseObject):
 
-    def __init__(self, node_id=None, fqdn=None,
-                 tags=None, status=None, sync_status=None, last_sync=None,
-                 updated_at=None,
+    def __init__(self, node_id=None, fqdn=None, ipv4_addr=None,
+                 tags=None, status=None, sync_status=None,
+                 last_sync=None, updated_at=None,
                  *args, **kwargs):
         super(NodeContext, self).__init__(*args, **kwargs)
         self.node_id = node_id or self._get_node_id() or self._create_node_id()
         self.fqdn = fqdn or socket.getfqdn()
-        self.ipv4_addr = socket.gethostbyname(self.fqdn)
+        self.ipv4_addr = ipv4_addr or socket.gethostbyname(self.fqdn)
 
         curr_tags = []
         try:
