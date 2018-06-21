1ca0a2ef0b38f4ff028631fed998ee312e229d7e
Nathan Weinberg <nathan2@stwmd.net>
2018-06-21 11:02:39 -0400

    Added new minor tests for objects to increase coverage

---
+++ tendrl/commons/tests/objects/node_alert/test_node_alert.py
@@ -0,0 +1,5 @@
+from tendrl.commons.objects.node_alert import NodeAlert 
+
+def test_render():
+	alert_obj = NodeAlert()
+	alert_obj.render()
---
+++ tendrl/commons/tests/objects/notification_only_alert/test_notification_only_alert.py
@@ -0,0 +1,5 @@
+from tendrl.commons.objects.notification_only_alert import NotificationOnlyAlert 
+
+def test_render():
+	noa_obj = NotificationOnlyAlert()
+	noa_obj.render()
