b79942bb52bfc2c5edeaa4f84af8908b755d0069
jeverett <jheverett1988@gmail.com>
2018-06-22 15:04:30 -0400

    check_cluster_available unit test

--- tendrl/commons/tests/objects/cluster/atoms/check_cluster_available/test_check_cluster_available_init.py
+++ tendrl/commons/tests/objects/cluster/atoms/check_cluster_available/test_check_cluster_available_init.py
@@ -1,17 +1,33 @@
 import etcd
 import maps
+import pytest
 from tendrl.commons.objects.cluster.atoms.check_cluster_available import CheckClusterAvailable  # noqa
-from tendrl.commons.objects.cluster import Cluster
+from tendrl.commons.objects import AtomExecutionFailedError
+
+
+class MockCluster(object):
+    def __init__(self, integration_id = 0):
+        self.is_managed = True
+    def load(self):
+        return self
+    def exists(self):
+        return self
+
 
 
 def test_check_cluster_available():
-    NS._int = maps.NamedDict
+    NS.publisher_id = 0
+    NS._int = maps.NamedDict()
     NS.tendrl = maps.NamedDict()
     NS.tendrl.objects = maps.NamedDict()
-    NS.tendrl.objects.Cluster = Cluster()
+    NS.tendrl.objects.Cluster = MockCluster
     test = CheckClusterAvailable()
     test.parameters = maps.NamedDict()
     test.parameters['TendrlContext.integration_id'] = \
         "7a3f2238-ef79-4943-9edf-762a80cf22a0"
+    test.parameters['job_id'] = 0
+    test.parameters['flow_id'] = 0
+    NS.tendrl_context = maps.NamedDict(integration_id="")
     NS._int.client = etcd.Client()
-    test.run()
+    with pytest.raises(AtomExecutionFailedError):
+        test.run()
