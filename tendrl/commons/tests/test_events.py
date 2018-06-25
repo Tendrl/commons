4cdbb1e4a034335e742cd6ccd9e564ad370ae424
mlanotte <mlanotte@redhat.com>
2018-06-25 09:30:34 -0400

    Fixed loss of coverage issue and added file Added slight coverage to a few files, and altered the test events and test_init so that there no longer was a loss of coverage in the __init__ file. I found the problem to be that the event test being called first was causing an issue because when isolating the test_init the code got covered. So to fix the issue I added the constructor test before the event test.

--- tendrl/commons/tests/test_events.py
+++ tendrl/commons/tests/test_events.py
@@ -1,7 +1,79 @@
 import __builtin__
+import etcd
+from etcd import Client
 import maps
+from mock import MagicMock
+from mock import patch
+
 from tendrl.commons.event import Event
 from tendrl.commons.message import Message
+from tendrl.commons.objects import BaseObject
+import tendrl.commons.objects.node_context as node
+import tendrl.commons.objects.tendrl_context as tendrl_context
+from tendrl.commons import TendrlNS
+from tendrl.commons.utils import etcd_utils
+
+
+# init that does not have the built in attribute
+# Code was not being covered when only in the test_init file
+# Some problem with the events test in this file was causing the
+# problem. So put this before that test so that it would be covered
+@patch.object(tendrl_context.TendrlContext, "load")
+@patch.object(etcd, "Client")
+@patch.object(Client, "read")
+@patch.object(node.NodeContext, '_get_node_id')
+@patch.object(etcd_utils, 'read')
+def init(patch_etcd_utils_read,
+         patch_get_node_id,
+         patch_read,
+         patch_client,
+         tc):
+    tc.return_value = tendrl_context.TendrlContext
+    patch_get_node_id.return_value = 1
+    patch_read.return_value = etcd.Client()
+    patch_client.return_value = etcd.Client()
+    # setattr(__builtin__, "NS", maps.NamedDict())
+    # setattr(NS, "_int", maps.NamedDict())
+    NS._int.etcd_kwargs = {
+        'port': 1,
+        'host': 2,
+        'allow_reconnect': True}
+    NS._int.client = etcd.Client(**NS._int.etcd_kwargs)
+    NS["config"] = maps.NamedDict()
+    NS.config["data"] = maps.NamedDict()
+    NS.config.data['tags'] = "test"
+    patch_etcd_utils_read.return_value = maps.NamedDict(
+        value='{"status": "UP",'
+              '"pkey": "tendrl-node-test",'
+              '"node_id": "test_node_id",'
+              '"ipv4_addr": "test_ip",'
+              '"tags": "[\\"my_tag\\"]",'
+              '"sync_status": "done",'
+              '"locked_by": "fd",'
+              '"fqdn": "",'
+              '"last_sync": "date"}')
+    with patch.object(etcd_utils, "read") as utils_read:
+        utils_read.return_value = maps.NamedDict(
+            value='{"tags":[]}'
+        )
+        with patch.object(BaseObject, "load") as node_load:
+            node.load = MagicMock()
+            node_load.return_value = node
+    tendrlNS = TendrlNS()
+    return tendrlNS
+
+
+def test_constructor():
+    with patch.object(TendrlNS, 'setup_common_objects') as \
+            mocked_method:
+        mocked_method.return_value = None
+        tendrlNS = TendrlNS()
+    tendrlNS = init()
+    # Default Parameter Testing
+    assert tendrlNS.ns_name == "tendrl"
+    assert tendrlNS.ns_src == "tendrl.commons"
+    # Check for existance and right data type
+    assert isinstance(NS, maps.NamedDict)
 
 
 # Event Dummys provide code coverage for the Event constructor
@@ -13,13 +85,6 @@ event_dummy = Event(Message(priority="info", publisher="node_agent", payload={
 event_dummy2 = Event(Message(priority="info", publisher="node_agent", payload={
     "message": TypeError}, node_id="Test id"))
 
-# event_test_write = Event(Message(priority="notice",
-# publisher="node_agent", payload={
-# "message":  "Test Message"}, node_id="Test id"))
-
-
-# def test_write():
-# event_test_write._write(message_new)
 
 def test_write():
     setattr(__builtin__, "NS", maps.NamedDict())
@@ -34,9 +99,3 @@ def test_write():
     event_to_test._write(message_new)
     event_to_test.socket_path = None
     event_to_test._write(message_new)
-    # event_to_test.socket_path = NS.config.data['logging_socket_path']
-    # event_to_test._pack_and_send(Message.to_json(message_new))
-
-# def test_start():
-#   from tendrl.commons.profiler import start
-#   start()
---
+++ tendrl/commons/tests/utils/test_monitoring_utils.py
@@ -0,0 +1,44 @@
+import __builtin__
+import maps
+from tendrl.commons.utils.monitoring_utils import delete_resource_from_graphite
+from tendrl.commons.utils.monitoring_utils import update_dashboard
+
+
+# Create a MockObject class, acts similarly to the patch object and mock
+# tools for testing.
+class MockJob(object):
+
+    def __init__(self, job_id=None, status=None, payload=None):
+        self.locked_by = None
+        self.status = status
+        self.payload = payload
+        self.job_id = job_id
+
+    def save(self):
+        pass
+
+
+# Tests that the update_dashboard method runs
+def test_update_dashboard():
+    setattr(__builtin__, "NS", maps.NamedDict())
+    NS.publisher_id = 1
+    NS.tendrl = maps.NamedDict()
+    NS.tendrl.objects = maps.NamedDict()
+    NS.tendrl_context = maps.NamedDict()
+    NS.tendrl_context.integration_id = 0
+    NS.tendrl_context.cluster_name = "Test Name"
+    NS.tendrl.objects.Job = MockJob
+    assert update_dashboard("Test Name", "Test", 0, "push")
+
+
+# Tests that the delete resource from graphite method runs
+def test_delete_resource_from_graphite():
+    setattr(__builtin__, "NS", maps.NamedDict())
+    NS.publisher_id = 1
+    NS.tendrl = maps.NamedDict()
+    NS.tendrl.objects = maps.NamedDict()
+    NS.tendrl_context = maps.NamedDict()
+    NS.tendrl_context.integration_id = 0
+    NS.tendrl_context.cluster_name = "Test Name"
+    NS.tendrl.objects.Job = MockJob
+    assert delete_resource_from_graphite("Test Name", "Test", 0, "push")
