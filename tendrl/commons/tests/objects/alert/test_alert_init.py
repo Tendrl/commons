4bcef9b01efc5c8a39d427f7a2c38a94045259bb
mlanotte <mlanotte@redhat.com>
2018-06-29 11:18:49 -0400

    Got alert.py to 100% covered partial if statements, also mocked a yaml path in the config file to get that to 100%

--- tendrl/commons/tests/objects/alert/test_alert_init.py
+++ tendrl/commons/tests/objects/alert/test_alert_init.py
@@ -7,6 +7,19 @@ from tendrl.commons.objects.alert import Alert
 from tendrl.commons.objects.alert import AlertUtils
 
 
+def test_alert_render():
+    setattr(__builtin__, "NS", maps.NamedDict())
+    setattr(NS, "_int", maps.NamedDict())
+    NS.publisher_id = "Test_id"
+    with patch.object(objects.BaseObject, 'load_definition',
+                      return_value=maps.NamedDict()):
+        alert_obj = Alert()
+        alert_obj.render()
+        # Makes test not partial
+        alert_obj.tags = "Test string"
+        alert_obj.render()
+
+
 def test_equals():
     setattr(__builtin__, "NS", maps.NamedDict())
     setattr(NS, "_int", maps.NamedDict())
@@ -31,6 +44,9 @@ def test_update():
         alert2.severity = "WARNING"
         alert_ex = AlertUtils()
         alert_ex.update(alert1, alert2)
+        alert1.severity = "CRITICAL"
+        # Makes first if statement false
+        alert_ex.update(alert1, alert2)
 
 
 def test_is_same():
@@ -47,6 +63,13 @@ def test_is_same():
         # Covers first false statement where resources are different
         assert alert_ex.is_same(alert1, alert2) is False
         alert2.resource = "Test Resource1"
+        alert1.tags = {"No plug": "Hi"}
+        alert1.classification = ["node"]
+        alert1.node_id = "node1"
+        alert2.classification = ["node"]
+        alert2.node_id = "node2"
+        # Makes second if false, not plugin in alert1
+        assert alert_ex.is_same(alert1, alert2) is False
         alert1.tags = {'plugin_instance': "Hi"}
         alert2.tags = {'plugin': "Ho"}
         # Covers if plugin is in alert1 but not alert2
@@ -81,6 +104,9 @@ def test_is_same():
         # alert1 clas has node, but alert2 does not
         assert alert_ex.is_same(alert1, alert2) is False
         alert1.classification = ["no_longer_the_same"]
+        alert2.classification = ["fail"]
+        # Alert1 clas does not have node, and neither does Alert2
+        assert alert_ex.is_same(alert1, alert2) is False
         alert2.classification = ["node"]
         # Alert1 clas does not have node, but alert2 does
         assert alert_ex.is_same(alert1, alert2) is False
